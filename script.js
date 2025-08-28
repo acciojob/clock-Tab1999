0ec756988a19e9b434ada88e8efbd86843b00a4a
Tab1999 <103740095+Tab1999@users.noreply.github.com>
2025-08-28 19:34:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function updateTimer() {
+  // Get current date & time
+  let now = new Date();
+
+  // Format it as readable string
+  let dateTime = now.toLocaleString();
+
+  // Display in timer <p>
+  document.getElementById("timer").textContent = dateTime;
+}
+
+// Call immediately so it doesn’t wait 1 sec
+updateTimer();
+
+// Update every second
+setInterval(updateTimer, 1000);
